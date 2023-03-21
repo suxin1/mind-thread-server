8fc8c12cc3b7767194554396af391b11972c9730
Ubuntu <ubuntu@ubuntu.lan>
2023-03-21 16:13:54 +0000

    allow cors

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -3,6 +3,14 @@ export default [
   'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
+  {
+    name: "strapi::cors",
+    config: {
+      enabled: true,
+      headers: '*',
+      origin: ['http://localhost:1337', 'http://lcoalhost:5173']
+    }
+  },
   'strapi::logger',
   'strapi::query',
   'strapi::body',
